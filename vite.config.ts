f501cc7191d25ee1ee6a75727171e935b0d94dd0
Henry <henrylaw2003@gmail.com>
2025-07-02 21:31:41 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/2dnano/',
   plugins: [react()],
-  base: "/2dnano_web",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
